2b6f9ee1d36a344fd352de517dd749a4e115f153
Raghav Sood <raghav16259@iiitd.ac.in>
2018-02-09 19:12:05 +0530

    Flight and Passenger created

---
+++ Flight.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class Flight {
+    public int f_id;
+    public int num;
+    public int[] status;
+    public Flight(){
+        Random random= new Random();
+        num=random.nextInt(100);
+        num++;
+    }
+}
