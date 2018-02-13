572c301f94c98db06da763ff3aec9209914e12f0
Raghav Sood <raghav16259@iiitd.ac.in>
2018-02-14 01:35:24 +0530

    Implemented serial execution of transactions

--- Concurrency_Control_Manager.java
+++ Concurrency_Control_Manager.java
@@ -1,15 +1,61 @@
 import java.io.*;
-public class Concurrency_Control_Manager
+import java.util.Random;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Concurrency_Control_Manager implements Runnable
 {
-    public static void main(String[] args)throws IOException
-    {
-        Flight[] flight=new Flight[5];
+    static volatile Flight[] flight=new Flight[5];
+    static Passenger[] passenger=new Passenger[10];
+    public static void main(String[] args) throws IOException, InterruptedException {
+
         for (int i=0;i<5;i++){
             flight[i]=new Flight(i+1);
         }
-        Passenger[] passenger=new Passenger[10];
+
         for (int i=0;i<10;i++){
             passenger[i]=new Passenger(i+1);
         }
+        ExecutorService exec = Executors.newFixedThreadPool(3);
+
+        for(int i=0;i<10;i++)
+        {
+            Concurrency_Control_Manager thread=new Concurrency_Control_Manager();
+            exec.execute(thread);
+        }
+        if(!exec.isTerminated())
+        {
+            exec.shutdown();
+            exec.awaitTermination(5L, TimeUnit.SECONDS);
+        }
+
+    }
+
+    @Override
+    public void run()
+    {
+        int counter=0;
+        while(counter<10)
+        {
+            counter++;
+            Random random=new Random();
+            int type=random.nextInt(5)+1;
+            int flight_id=random.nextInt(5)+1;
+            int passenger_id=random.nextInt(10)+1;
+            int flight_id_2=0;
+            if(type==5)
+            {
+                flight_id_2=random.nextInt(5)+1;
+                while (flight_id_2==flight_id)
+                    flight_id_2=random.nextInt(5)+1;
+            }
+            synchronized(this)
+            {
+                Transaction transaction=new Transaction(flight,passenger,type,flight_id,passenger_id,flight_id_2);
+            }
+        }
+
     }
 }
--- Transaction.java
+++ Transaction.java
@@ -15,50 +15,69 @@ public class Transaction {
         switch (type)
         {
             case 1: reserve(flight_id,passenger_id);
-            break;
+                break;
             case 2: cancel(flight_id,passenger_id);
                 break;
             case 3: my_flights(passenger_id);
                 break;
-            case 4: total_reservations();
+            case 4: System.out.println("count= "+total_reservations());
                 break;
             case 5: transfer(flight_id,flight_id_2,passenger_id);
                 break;
         }
     }
     public void reserve(int flight_id,int passenger_id){
-    	for (int i=0;i<flights[flight_id-1].num;i++){
-    		if (flights[flight_id-1].seats[i].passenger_id==0){
-    			flights[flight_id-1].seats[i].passenger_id=passenger_id;
-    			passenger[passenger_id-1].booked.add(flights[flight_id-1].seats[i]);
-    			break;
-    		}
-    	}
+        for (int i=0;i<flights[flight_id-1].num;i++){
+            if (flights[flight_id-1].seats[i].passenger_id==0){
+                flights[flight_id-1].seats[i].passenger_id=passenger_id;
+                passenger[passenger_id-1].booked.add(flights[flight_id-1].seats[i]);
+                break;
+            }
+        }
     }
     public void cancel(int flight_id,int passenger_id){
-    	for (int i=0;i<flights[flight_id-1].num;i++){
-    		if (flights[flight_id-1].seats[i].passenger_id==passenger_id){
-    			flights[flight_id-1].seats[i].passenger_id=0;
-    			passenger[passenger_id-1].booked.remove(flights[flight_id-1].seats[i]);
-    		}
-    	}
+        for (int i=0;i<flights[flight_id-1].num;i++){
+            if (flights[flight_id-1].seats[i].passenger_id==passenger_id){
+                flights[flight_id-1].seats[i].passenger_id=0;
+                passenger[passenger_id-1].booked.remove(flights[flight_id-1].seats[i]);
+            }
+        }
     }
     public void my_flights(int passenger_id){
-    	ArrayList<Seat> p=passenger[passenger_id-1].booked;
-    	for(int i=0;i<p.size();i++){
-    		System.out.println(p.get(i).flight_id+" "+p.get(i).seat_number);
-    	}
+        ArrayList<Seat> p=passenger[passenger_id-1].booked;
+        for(int i=0;i<p.size();i++){
+            System.out.println(p.get(i).flight_id+" "+p.get(i).seat_number);
+        }
     }
     public int total_reservations(){
-    	int ct=0;
-    	for (int i=0;i<5;i++){
-    		for (int j=0;j<flights[i].num;i++){
-    			if (flights[i].seats[j].passenger_id!=0)ct++;
-    		}
-    	}
-    	return ct;
+        int count=0;
+        for (int i=0;i<5;i++){
+            for (int j=0;j<flights[i].num;j++){
+                if (flights[i].seats[j].passenger_id!=0)
+                    count++;
+            }
+        }
+        return count;
     }
-    public void transfer(int flight_id_1,int flight_id_2,int passenger_id){
-    	
+    public void transfer(int flight_id_1,int flight_id_2,int passenger_id)
+    {
+        Flight f1=flights[flight_id_1-1];
+        Flight f2=flights[flight_id_2-1];
+        for (int i=0;i<f1.num;i++)
+        {
+            if (f1.seats[i].passenger_id == passenger_id)
+            {
+                for (int j=0;j<f2.num;j++){
+                    if (f2.seats[j].passenger_id==0)
+                    {
+                        flights[flight_id_1-1].seats[i].passenger_id=0;
+                        passenger[passenger_id-1].booked.remove(flights[flight_id_1-1].seats[i]);
+                        flights[flight_id_2-1].seats[j].passenger_id=passenger_id;
+                        passenger[passenger_id-1].booked.add(flights[flight_id_2-1].seats[i]);
+                    }
+                }
+            }
+        }
+
     }
 }
